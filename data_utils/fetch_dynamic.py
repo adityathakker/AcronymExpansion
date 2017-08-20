856886c8a2838c0ef6c765e6e5786d0ee7faa379
adityathakker <aditya@adityathakker.com>
2017-08-20 07:40:06 +0530

    Dynamic Method Updated

--- data_utils/fetch_dynamic.py
+++ data_utils/fetch_dynamic.py
@@ -3,14 +3,36 @@ import wikipedia
 from bs4 import BeautifulSoup
 import requests
 import json
-from config import *
+# from config import *
 import re
-from data_utils.get_acronyms import *
+# from get_acronyms import *
 
-DATA_FILE_PATH = "../" + DATA_FILE_PATH
-META_FILE_PATH = "../" + META_FILE_PATH
+DATA_FILE_PATH = "../" + "data/acronyms.json"
+#META_FILE_PATH = "../" + META_FILE_PATH
 SEARCH_URL = "https://en.wikipedia.org/w/index.php?title=Special:Search&go=Go&search="
 DISAMBIGUATION_URL = "https://en.wikipedia.org/wiki/%s_(disambiguation)"
+
+def get_doc(url):
+    text = list()
+    if url == "":
+        #print(url)
+        return ""
+    response = requests.get(url)
+    soup = BeautifulSoup(markup=response.text, features="lxml")
+    if soup is None:
+        #print(url)
+        return ""
+    content = soup.find(name="div", attrs={"class": "mw-parser-output"})
+    if content is None:
+        #print(url)
+        return ""
+    list_p = content.findAll(name="p")
+    if list_p is None:
+        #print(url)
+        return ""
+    for p in list_p:
+        text.append(str(p.text))
+    return " ".join(text)
 
 
 def get_pages(query):
@@ -41,40 +63,47 @@ def get_pages(query):
 
 
 def find_full_form(abbr, para):
-    re_string = ""
+    re_string = "\b"
     for c in abbr:
-        re_string = re_string + c + "\w+ "
+        re_string = re_string + c + "\w+[ _-]"
+    re_string = re_string[:len(re_string)-1-4]
+    print(re_string)
     return re.findall(re_string, para)
 
 
 def get_acronyms(query):
     acronyms = list()
-    query = query.lower()
 
     response = requests.get(DISAMBIGUATION_URL % str(query))
+    print(DISAMBIGUATION_URL % str(query))
+
+    query = query.lower()
     if response.status_code != 404:
         print("Disambiguation Page Exists :D")
         soup = BeautifulSoup(markup=response.text, features="lxml")
         if soup is None:
             return None
-        div = soup.find("div", attrs={"class": "mw-content-text"})
-        all_uls = div.find_all("ul")
+        div = soup.find("div", attrs={"class": "mw-parser-output"})
+        all_uls = div.findAll("ul")
 
         for ul in all_uls:
-            all_lis = ul.find_all("li")
+            all_lis = ul.findAll("li")
             for li in all_lis:
                 a = li.find("a")
+                if a is None:
+                    continue
                 url = "https://en.wikipedia.org" + a["href"]
                 content = get_doc(url)
                 full_forms = find_full_form(query, content)
 
                 for item in full_forms:
-                    print(item)
+                    print("Item: %s" % item)
                     possible_full_form = dict()
                     possible_full_form["full_form"] = item.strip()
                     possible_full_form["summary"] = content
                     acronyms.append(possible_full_form)
-    else:
+    if True:
+        print("No Disambiguation Page Found. Normally Searching...")
         results = wikipedia.search(query=query, results=10)
         print(results)
         if len(results) <= 0:
@@ -88,7 +117,7 @@ def get_acronyms(query):
             full_forms = find_full_form(query, summary)
 
             for item in full_forms:
-                print(item)
+                print("Item:: %s" % item)
                 possible_full_form = dict()
                 possible_full_form["full_form"] = item.strip()
                 possible_full_form["summary"] = summary
@@ -127,6 +156,10 @@ def findBestLongForm(shortForm, longForm):
     return longForm[:spIndex]
 
 
+
+print(get_acronyms("FDS"))
+
+
 acronyms = json.load(open(DATA_FILE_PATH, mode="r"))
 print("Existing File Loaded...")
 print("Total Acronyms: %s" % len(acronyms))
@@ -135,11 +168,14 @@ for item in acronyms:
     print("Acronyms for %s" % item)
     possibilities = get_acronyms(item)
     if possibilities is None or len(possibilities) == 0:
-        del acronyms[item]
+        # del acronyms[item]
+        print("No Possibilities :(")
         continue
 
     acronyms[item]["possibilities"] = possibilities
     print(possibilities)
     print("%s More to go" % (len(acronyms) - i))
     i += 1
-    print()
+    print("\n\n")
+
+json.dump(acronyms, open("../data/new_acronyms.json", "w"))
