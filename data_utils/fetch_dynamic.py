0fd09203d77ddfe1c48e4de2e30a92a195cc7e9d
Aditya Thakker <adityathakker02@gmail.com>
2017-08-20 06:37:41 +0530

    Added Disambiguation Method

--- data_utils/fetch_dynamic.py
+++ data_utils/fetch_dynamic.py
@@ -5,10 +5,12 @@ import requests
 import json
 from config import *
 import re
+from data_utils.get_acronyms import *
 
 DATA_FILE_PATH = "../" + DATA_FILE_PATH
 META_FILE_PATH = "../" + META_FILE_PATH
 SEARCH_URL = "https://en.wikipedia.org/w/index.php?title=Special:Search&go=Go&search="
+DISAMBIGUATION_URL = "https://en.wikipedia.org/wiki/%s_(disambiguation)"
 
 
 def get_pages(query):
@@ -49,24 +51,48 @@ def get_acronyms(query):
     acronyms = list()
     query = query.lower()
 
-    results = wikipedia.search(query=query, results=10)
-    print(results)
-    if len(results) <= 0:
-        raise Exception
+    response = requests.get(DISAMBIGUATION_URL % str(query))
+    if response.status_code != 404:
+        print("Disambiguation Page Exists :D")
+        soup = BeautifulSoup(markup=response.text, features="lxml")
+        if soup is None:
+            return None
+        div = soup.find("div", attrs={"class": "mw-content-text"})
+        all_uls = div.find_all("ul")
 
-    for each_result in results:
-        try:
-            summary = str(wikipedia.summary(each_result)).lower()
-        except Exception:
-            continue
-        full_forms = find_full_form(query, summary)
+        for ul in all_uls:
+            all_lis = ul.find_all("li")
+            for li in all_lis:
+                a = li.find("a")
+                url = "https://en.wikipedia.org" + a["href"]
+                content = get_doc(url)
+                full_forms = find_full_form(query, content)
 
-        for item in full_forms:
-            print(item)
-            possible_full_form = dict()
-            possible_full_form["full_form"] = item.strip()
-            possible_full_form["summary"] = summary
-            acronyms.append(possible_full_form)
+                for item in full_forms:
+                    print(item)
+                    possible_full_form = dict()
+                    possible_full_form["full_form"] = item.strip()
+                    possible_full_form["summary"] = content
+                    acronyms.append(possible_full_form)
+    else:
+        results = wikipedia.search(query=query, results=10)
+        print(results)
+        if len(results) <= 0:
+            return None
+
+        for each_result in results:
+            try:
+                summary = str(wikipedia.summary(each_result)).lower()
+            except Exception:
+                continue
+            full_forms = find_full_form(query, summary)
+
+            for item in full_forms:
+                print(item)
+                possible_full_form = dict()
+                possible_full_form["full_form"] = item.strip()
+                possible_full_form["summary"] = summary
+                acronyms.append(possible_full_form)
 
     return acronyms
 
@@ -108,6 +134,10 @@ i = 1
 for item in acronyms:
     print("Acronyms for %s" % item)
     possibilities = get_acronyms(item)
+    if possibilities is None or len(possibilities) == 0:
+        del acronyms[item]
+        continue
+
     acronyms[item]["possibilities"] = possibilities
     print(possibilities)
     print("%s More to go" % (len(acronyms) - i))
